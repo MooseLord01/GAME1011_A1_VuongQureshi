2a867ba4e853b6997213ced89b13b07d7bd6f740
Dat <Davidvuong67@gmail.com>
2021-02-11 18:24:52 -0500

    Worked on NonGamingStudent.h/.cpp & fixed code
    Worked on NonGamingStudent.h/.cpp and added first/last name arrays

--- GAME1011_A1_VuongQureshi/GAME1011_A!_VuongQureshi/NonGamingStudent.h
+++ GAME1011_A1_VuongQureshi/GAME1011_A!_VuongQureshi/NonGamingStudent.h
@@ -1,1 +1,41 @@
 #pragma once
+#ifndef __NONGAMINGSTUDENT_H__
+#define __NONGAMINGSTUDENT_H__
+#include "Student.h"
+
+class NonGamingStudent : public Student
+{
+private:
+
+	//Streaming Services Array
+	std::string listofStreamingServices[14] = {
+		"Netflix",
+		"Disney+",
+		"Twitch",
+		"YouTube",
+		"Crunchyroll",
+		"Hulu",
+		"Amazon Prime Video",
+		"FireFox", "HBO Max",
+		"Sling TV",
+		"Vudu",
+		"ESPN+",
+		"Apple TV"
+	};
+
+	std::string streamingServices;
+	int streamHours;
+
+public:
+	NonGamingStudent(std::string name);
+	void getInformation() override;
+
+	//Setters
+	void setStreamServices(std::string service);
+	void setStreamHours(int hours);
+
+	//Getters
+	std::string getStreamServices();
+	int getStreamHours();
+};
+#endif
